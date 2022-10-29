3c331f3346228ce20a01cd671dc286cb8d85f5a3
Kevin A. Hernández Rostrán <kevinah95@gmail.com>
2022-10-29 01:24:43 -0600

    - Add cors

--- pages/api/commits.js
+++ pages/api/commits.js
@@ -1,19 +1,27 @@
 import clientPromise from "../../lib/mongodb";
+import NextCors from 'nextjs-cors';
 
 export default async (req, res) => {
-   try {
-       const client = await clientPromise;
-       const db = client.db("test_database");
+    await NextCors(req, res, {
+        // Options
+        methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE'],
+        origin: '*',
+        optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
+    });
 
-       const movies = await db
-           .collection("rm_commit")
-           .find({})
-           .sort({ committer_date: -1 })
-           //.limit(100)
-           .toArray();
+    try {
+        const client = await clientPromise;
+        const db = client.db("test_database");
 
-       res.json(movies);
-   } catch (e) {
+        const movies = await db
+            .collection("rm_commit")
+            .find({})
+            .sort({ committer_date: -1 })
+            //.limit(100)
+            .toArray();
+
+        res.json(movies);
+    } catch (e) {
         res.status(code).send(e);
-   }
+    }
 };
