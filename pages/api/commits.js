f6108a088dce3e672bdb39f8211f3c70cea75517
Kevin A. Hernández Rostrán <kevinah95@gmail.com>
2022-10-28 23:15:19 -0600

    - Add pnpm. - Add commits endpoint.

---
+++ pages/api/commits.js
@@ -0,0 +1,19 @@
+import clientPromise from "../../lib/mongodb";
+
+export default async (req, res) => {
+   try {
+       const client = await clientPromise;
+       const db = client.db("test_database");
+
+       const movies = await db
+           .collection("rm_commit")
+           .find({})
+           .sort({ committer_date: -1 })
+           //.limit(10)
+           .toArray();
+
+       res.json(movies);
+   } catch (e) {
+       console.error(e);
+   }
+};
